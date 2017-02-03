670f13f40f1054d45f6cd464d7651de75f115925
Tim Allen <tim@noblesamurai.com>
2017-02-03 16:25:33 +1100

    Firt cut.

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
-const Canvas = require('isomorphic-canvas');
+const Canvas = require('canvas');
+
 const getCanvasPixels = require('get-canvas-pixels');
+const imageType = require('image-type');
+const getPixels = require('get-pixels');
+require('easy-profiler');
 
 /*
  * @return Promise<uint8Array>
  */
 module.exports = function (frame) {
-  console.log(frame);
+  let mode;
+  let canvas;
+  function setMode (name) {
+    mode = name;
+    EP.begin(mode); // profiling
+  }
   if (frame.constructor.name === 'Canvas') {
-    return Promise.resolve(getCanvasPixels(frame));
-  } else throw new Error ('unknown thing');
+    setMode('canvas');
+    canvas = frame;
+  } else {
+    let type = imageType(frame);
+    if (type) setMode('image');
+    canvas = imageToCanvas(frame);
+  }
+  if (!mode) {
+    throw new Error ('unknown input format');
+  }
+
+  var result = getCanvasPixels(canvas);
+  EP.end(mode);
+  return Promise.resolve(result);
 };
+
+function imageToCanvas (frame) {
+  let img = new Canvas.Image();
+  img.src = frame;
+  let c = new Canvas(img.width, img.height);
+  let ctx = c.getContext('2d');
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  return c;
+}
