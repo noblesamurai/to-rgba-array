4b4d82a071b3d09e61d7ec6de03735983c164331
Tim Allen <tim@noblesamurai.com>
2017-02-10 14:16:44 +1100

    Further improve tests.

--- test/index.js
+++ test/index.js
@@ -2,25 +2,42 @@ const expect = require('chai').expect;
 const toRGBAArray = require('..');
 const Canvas = require('canvas');
 const PNG = require('pngjs').PNG;
+const ImageData = Canvas.ImageData;
 
 describe('to-rgba-array', function () {
+  let testImage = Buffer.from(
+    [ 200, 100, 200, 1, 100, 200, 200, 255, 200, 100, 200, 255,
+      200, 100, 200, 255, 100, 200, 200, 255, 200, 100, 200, 255,
+      200, 100, 200, 255, 100, 200, 200, 255, 200, 100, 200, 255
+    ]);
   it('will work with a canvas', function () {
-    const width = 200;
-    const height = 200;
-    var canvas = new Canvas(width, height);
-    return toRGBAArray(canvas).then(function (result) {
-      expect(result).to.be.a('Uint8ClampedArray');
-      expect(result.length).to.equal(width * height * 4);
-    });
-  });
-
-  it('will work with a PNG image', function () {
-    let testImage = Buffer.from(
-      [ 200, 100, 200, 1, 100, 200, 200, 255, 200, 100, 200, 255,
+    // Note that there is a slight difference between the input and output for
+    // the non 255 alpha value (pixel 1).
+    // This is due to the fact that we are using a canvas and the
+    // pre-multplication of alpha values and reversal thereof is a lossy process.
+    // Here we are going to canvas twice.., hence the three zeros in a row for
+    // pixel one.
+    // In practice I don't think this will be a major problem.
+    let expectedImage = Buffer.from(
+      [ 0, 0, 0, 1, 100, 200, 200, 255, 200, 100, 200, 255,
         200, 100, 200, 255, 100, 200, 200, 255, 200, 100, 200, 255,
         200, 100, 200, 255, 100, 200, 200, 255, 200, 100, 200, 255
       ]);
 
+    const width = 3;
+    const height = 3;
+    let canvas = new Canvas(width, height);
+    let ctx = canvas.getContext('2d');
+    let imageData = new ImageData(new Uint8ClampedArray(testImage), ctx.canvas.width, ctx.canvas.height);
+    ctx.putImageData(imageData, 0, 0);
+
+    return toRGBAArray(canvas).then(function (result) {
+      expect(Buffer.from(result).length).to.equal(testImage.length);
+      expect(Buffer.from(result)).to.eql(expectedImage);
+    });
+  });
+
+  it('will work with a PNG image', function () {
     // Note that there is a slight difference between the input and output for
     // the non 255 alpha value (pixel 1).
     // This is due to the fact that we are using a canvas and the
